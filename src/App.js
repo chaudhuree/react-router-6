3d9822bf258c9e466c51fff51103591f3b73e83c
chaudhuree <chaudhuree00@gmail.com>
2022-05-13 20:42:59 +0600

    docs: first page or basic startup coding

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 function App() {
-  return <h2>React Router 6 Tutorial</h2>;
+  return <Router>
+    <Routes>
+      <Route path='/' element={<div>Home</div>}></Route>
+      <Route path='testing' element={<h2>testing</h2>}></Route>
+    </Routes>
+  </Router>;
 }
 
 export default App;
