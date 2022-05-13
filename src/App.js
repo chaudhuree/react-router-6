1a8b818dcc9f76078db98206ddc1eaba05f634c1
chaudhuree <chaudhuree00@gmail.com>
2022-05-13 22:02:45 +0600

    docs: styledNavbar with className conditioned

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
-import { StyledNavbar } from './components/StyledNavbar';
+// import { StyledNavbar } from './components/StyledNavbar';
 import About from './pages/About';
 import Error from './pages/Error';
 import Home from './pages/Home';
@@ -9,7 +9,7 @@ import { SharedLayout } from './pages/SharedLayout';
 function App() {
   return <Router>
     <Navbar/>
-    <StyledNavbar/>
+    {/* <StyledNavbar/> */}
     <Routes>
       <Route path='/' element={<SharedLayout />}>
       {/* index page */}
--- src/components/StyledNavbar.js
+++ src/components/StyledNavbar.js
@@ -1,29 +1,65 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+// import React from 'react'
+// import { NavLink } from 'react-router-dom'
 
-export const StyledNavbar = () => {
+// export const StyledNavbar = () => {
+//   return (
+//     <nav className='navbar'>
+//       <NavLink to='/' className='btn' style={({ isActive }) => {
+//         return { color: isActive ? 'white' : 'red', background: isActive ? 'black' : 'white' }
+//       }}>Home</NavLink>
+
+//       <NavLink to='/about' className='btn'
+//         style={({ isActive }) => {
+//           return { color: isActive ? 'white' : 'red', background: isActive ? 'black' : 'white' }
+//         }}>About</NavLink>
+
+//       <NavLink to='/products' className='btn'
+//         style={({ isActive }) => {
+//           return { color: isActive ? 'white' : 'red', background: isActive ? 'black' : 'white' }
+//         }}>Products</NavLink>
+
+//       <NavLink to='/first' className='btn'
+//         style={({ isActive }) => {
+//           return { color: isActive ? 'white' : 'red', background: isActive ? 'black' : 'white' }
+//         }}>First Page</NavLink>
+        
+//       <NavLink to='/anything' className='link'>Error Page</NavLink>
+
+//     </nav>
+//   )
+// }
+
+// 4 another way to do it 
+
+import { NavLink } from 'react-router-dom';
+const Navbar = () => {
   return (
     <nav className='navbar'>
-      <NavLink to='/' className='btn' style={({ isActive }) => {
-        return { color: isActive ? 'white' : 'red', background: isActive ? 'black' : 'white' }
-      }}>Home</NavLink>
-      <NavLink to='/about' className='btn'
-        style={({ isActive }) => {
-          return { color: isActive ? 'white' : 'red', background: isActive ? 'black' : 'white' }
-        }}>About</NavLink>
-
-      <NavLink to='/products' className='btn'
-        style={({ isActive }) => {
-          return { color: isActive ? 'white' : 'red', background: isActive ? 'black' : 'white' }
-        }}>Products</NavLink>
-
-      <NavLink to='/first' className='btn'
-        style={({ isActive }) => {
-          return { color: isActive ? 'white' : 'red', background: isActive ? 'black' : 'white' }
-        }}>First Page</NavLink>
-        
-      <NavLink to='/anything' className='link'>Error Page</NavLink>
-
+      <NavLink
+        to='/'
+        className={({ isActive }) => (isActive ? 'link active' : 'link')}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to='/about'
+        className={({ isActive }) => (isActive ? 'link active' : 'link')}
+      >
+        About
+      </NavLink>
+      <NavLink
+        to='/products'
+        className={({ isActive }) => (isActive ? 'link active' : 'link')}
+      >
+        Products
+      </NavLink>
+      <NavLink
+        to='/login'
+        className={({ isActive }) => (isActive ? 'link active' : 'link')}
+      >
+        Login
+      </NavLink>
     </nav>
-  )
-}
+  );
+};
+export default Navbar;
