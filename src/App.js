add31c5a453d540a6be557727a6806470849f488
chaudhuree <chaudhuree00@gmail.com>
2022-05-13 20:54:42 +0600

    docs: Link

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
 import About from './pages/About';
 import Home from './pages/Home';
 import Products from './pages/Products';
 function App() {
   return <Router>
+  <Link to='/home' className='btn'>Home</Link>
+  <Link to='/about' className='btn'>About</Link>
+  <Link to='/products' className='btn'>Products</Link>
+  <Link to='/' className='btn'>First Page</Link>
+
     <Routes>
       <Route path='/' element={<div>First page</div>}></Route>
       <Route path='home' element={<Home/>}/>
