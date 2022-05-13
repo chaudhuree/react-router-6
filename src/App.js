50bbd089d6a831bec7bc79cd8938af33b8a443be
chaudhuree <chaudhuree00@gmail.com>
2022-05-13 21:06:19 +0600

    docs: link and basic routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
 import About from './pages/About';
+import Error from './pages/Error';
 import Home from './pages/Home';
 import Products from './pages/Products';
 function App() {
   return <Router>
-  <Link to='/home' className='btn'>Home</Link>
+  <Link to='/' className='btn'>Home</Link>
   <Link to='/about' className='btn'>About</Link>
   <Link to='/products' className='btn'>Products</Link>
-  <Link to='/' className='btn'>First Page</Link>
+  <Link to='/first' className='btn'>First Page</Link>
+  <Link to='/anything' className='btn'>Error Page</Link>
 
     <Routes>
-      <Route path='/' element={<div>First page</div>}></Route>
-      <Route path='home' element={<Home/>}/>
+      <Route path='/' element={<Home/>}/>
+      <Route path='first' element={<div>First page</div>}></Route>
       <Route path='about' element={<About/>}/>
       <Route path='products' element={<Products/>}/>
+      <Route path='*' element={<Error/>}/>
     </Routes>
   </Router>;
 }
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -2,6 +2,7 @@ const Error = () => {
   return (
     <section className='section'>
       <h2>Error</h2>
+      <p> 404 - Page not found</p>
     </section>
   );
 };
